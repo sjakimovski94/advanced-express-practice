1f764edcbeb37558b5c7b5dd18abb764882aaaea
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-11-11 17:29:57 -0600

    Updated CommentController.js v1

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -1,35 +1,52 @@
 let comments = require("../comments");
+let CommentModel = require("../models/CommentModel")
 
 module.exports.list =  function list(request, response) {
     //return response.json([]);
-    return response.json(comments);
+    CommentModel.find().exec().then(comments=>{
+        // return response.json(vehicles)
+        return response.json(comments);
+       })
    }
    module.exports.show =  function show(request, response) {
     //return response.json({theId: request.params.id});
-    let comment = comments.find((u)=> u._id == request.params.id);
-    return response.json(comment);
+    CommentModel.findById(request.params.id).exec()
+    .then(comment=>{
+      return response.json(comment);
+    })
+    //let comment = comments.find((u)=> u._id == request.params.id);
    }
    module.exports.create =  function create(request, response) {
     //return response.json({});
-    request.body._id = comments.length+1;
-    request.body.postId = 1;
-    comments.push(request.body);
+    //request.body._id = comments.length+1;
+    //request.body.postId = 1;
+    //comments.push(request.body);
+    let comment = new CommentModel(request.body)
+    comment.save();
     return response.json(comments);
    }
    module.exports.update =  function update(request, response) {
     //return response.json({theId: request.params.id});
-    let updateComment = comments.find((comment)=> comment._id == request.params.id);
-
-    updateComment.likes = 1;
-
-    return response.json(comments);
+    CommentModel.findById(request.params.id).exec()
+    .then(comment=>{
+      comment.likes=1;
+      comment.save();
+      return response.json(comment);
+    })
+    //let updateComment = comments.find((comment)=> comment._id == request.params.id)
+    //updateComment.likes = 1;
+    //return response.json(comments);
    }
    module.exports.remove =  function remove(request, response) {
     //return response.json({});
-    let deleteComment = comments.find((comment)=> comment._id == request.params.id);
-
-    deleteComment.isActive= false;
-
-    return response.json(comments);
+    CommentModel.findById(request.params.id).exec()
+    .then(comment=>{
+      comment.isActive=false;
+      comment.save();
+      return response.json(comment);
+    })
+    //let deleteComment = comments.find((comment)=> comment._id == request.params.id);
+    //deleteComment.isActive= false;
+    //return response.json(comments);
    }
    
