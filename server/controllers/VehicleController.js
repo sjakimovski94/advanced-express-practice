847f38488c57b734b0db441b0ba3daf15f636d7a
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-11-11 16:37:46 -0600

    Updated VehicleControler.js v1

--- server/controllers/VehicleController.js
+++ server/controllers/VehicleController.js
@@ -1,35 +1,58 @@
 let vehicles = require("../vehicles");
+let VehicleModel = require("../models/VehicleModel")
 
 module.exports.list =  function list(request, response) {
     //return response.json([]);
+    VehicleModel.find().exec().then(vehicles=>{
+    // return response.json(vehicles)
     return response.json(vehicles);
-   }
+   })
+}
    module.exports.show =  function show(request, response) {
     //return response.json({theId: request.params.id});
-    let vehicle = vehicles.find((u)=> u._id == request.params.id);
-    return response.json(vehicle);
+    VehicleModel.findById(request.params.id).exec()
+      .then(vehicle=>{
+        return response.json(vehicle);
+      })
+    //let vehicle = vehicles.find((u)=> u._id == request.params.id);
+
    }
    module.exports.create =  function create(request, response) {
     //return response.json({});
-    request.body._id = vehicles.length+1;
-    request.body.postId = 1;
-    vehicles.push(request.body);
+    let vehicle = new VehicleModel(request.body)
+    vehicle.save();
+    //request.body._id = vehicles.length+1;
+    //request.body.postId = 1;
+    //vehicles.push(request.body);
     return response.json(vehicles);
    }
    module.exports.update =  function update(request, response) {
     //return response.json({theId: request.params.id});
-    let updateVehicle = vehicles.find((vehicle)=> vehicle._id == request.params.id);
+    VehicleModel.findById(request.params.id).exec()
+    .then(vehicle=>{
+      vehicle.isCPO=true;
+      vehicle.save();
+      return response.json(vehicle);
+    })
+    //let updateVehicle = vehicles.find((vehicle)=> vehicle._id == request.params.id);
 
-    updateVehicle.isCPO = true;
+    //updateVehicle.isCPO = true;
 
-    return response.json(vehicles);
+    //return response.json(vehicles);
    }
    module.exports.remove =  function remove(request, response) {
     //return response.json({});
-    let deleteVehicle = vehicles.find((vehicle)=> vehicle._id == request.params.id);
+    VehicleModel.findById(request.params.id).exec()
+    .then(vehicle=>{
+      vehicle.isActive=false;
+      vehicle.save();
+      return response.json(vehicle);
+    })
 
-    deleteVehicle.isActive= false;
+    //let deleteVehicle = vehicles.find((vehicle)=> vehicle._id == request.params.id);
 
-    return response.json(vehicles);
+    //deleteVehicle.isActive= false;
+
+    //return response.json(vehicles);
    }
    
