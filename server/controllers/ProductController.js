61fdb119b1c90e30c98da7176e97d911064f1ace
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-11-11 18:09:37 -0600

    Updated ProductController.js v1

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -1,35 +1,52 @@
 let products = require("../products");
+let ProductModel = require("../models/ProductModel")
 
 module.exports.list =  function list(request, response) {
     //return response.json([]);
-    return response.json(products);
+    ProductModel.find().exec().then(products=>{
+        // return response.json(vehicles)
+        return response.json(products);
+       })
    }
    module.exports.show =  function show(request, response) {
     //return response.json({theId: request.params.id});
-    let product = products.find((u)=> u._id == request.params.id);
-    return response.json(product);
+    ProductModel.findById(request.params.id).exec()
+    .then(product=>{
+      return response.json(product);
+    })
+    //let product = products.find((u)=> u._id == request.params.id);
    }
    module.exports.create =  function create(request, response) {
     //return response.json({});
-    request.body._id = products.length+1;
-    request.body.postId = 1;
-    products.push(request.body);
+    //request.body._id = products.length+1;
+    //request.body.postId = 1;
+    //products.push(request.body);
+    let product = new ProductModel(request.body)
+    product.save();
     return response.json(products);
    }
    module.exports.update =  function update(request, response) {
     //return response.json({theId: request.params.id});
-    let updateProduct = products.find((product)=> product._id == request.params.id);
-
-    updateProduct.inStock = true;
-
-    return response.json(products);
+    ProductModel.findById(request.params.id).exec()
+    .then(product=>{
+      product.inStock=true;
+      product.save();
+      return response.json(product);
+    })
+    //let updateProduct = products.find((product)=> product._id == request.params.id);
+    //updateProduct.inStock = true;
+    //return response.json(products);
    }
    module.exports.remove =  function remove(request, response) {
     //return response.json({});
-    let deleteProduct = products.find((product)=> product._id == request.params.id);
-
-    deleteProduct.isActive= false;
-
-    return response.json(products);
+    ProductModel.findById(request.params.id).exec()
+    .then(product=>{
+      product.isActive=false;
+      product.save();
+      return response.json(product);
+    })
+    //let deleteProduct = products.find((product)=> product._id == request.params.id);
+    //deleteProduct.isActive= false;
+    //return response.json(products);
    }
    
